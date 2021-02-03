2a461d140d8444d1495558c47ba70bbde0504828
0xLeif <zmeriksen@icloud.com>
2021-02-02 20:40:50 -0600

    Updated to have fonts

--- Tests/ScreenDataNavigationTests/ScreenDataNavigationTests.swift
+++ Tests/ScreenDataNavigationTests/ScreenDataNavigationTests.swift
@@ -13,9 +13,9 @@ final class ScreenDataNavigationTests: XCTestCase {
                                 subtitle: nil,
                                 backgroundColor: SomeColor(red: 0, green: 0, blue: 0),
                                 headerView: SomeView(type: .container, someContainer: SomeContainerView(isScrollable: true, axis: .vertical, views: [
-                                    SomeView(type: .label, someLabel: SomeLabel(title: "Hello World", subtitle: nil, style: nil, destination: Destination(type: .url, toID: "https://github.com/ServerDriven/ScreenData")))
+                                    SomeView(type: .label, someLabel: SomeLabel(title: "Hello World", subtitle: nil, font: .largeTitle, style: nil, destination: Destination(type: .url, toID: "https://github.com/ServerDriven/ScreenData")))
                                 ], style: nil)),
-                                someView: SomeView(type: .label, someLabel: SomeLabel(title: "Hello World", subtitle: nil, style: nil, destination: Destination(type: .screen, toID: "/some/data/5"))))
+                                someView: SomeView(type: .label, someLabel: SomeLabel(title: "Hello World", subtitle: nil, font: .largeTitle, style: nil, destination: Destination(type: .screen, toID: "/some/data/5"))))
         
         XCTAssertEqual(destinations.map { $0.toID },
                        screen.destinations.map { $0.toID })
