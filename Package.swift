7905ebb625c32aecae8cafc6c64345e78b23d52e
0xLeif <zmeriksen@icloud.com>
2021-02-02 20:39:28 -0600

    Updated to SD@0.3.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "ScreenData", url: "https://github.com/ServerDriven/ScreenData-swift", from: "0.2.4")
+        .package(name: "ScreenData", url: "https://github.com/ServerDriven/ScreenData-swift", from: "0.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
