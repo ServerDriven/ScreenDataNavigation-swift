6f996d7d52b3a888d6bf7d7bb6cd441fe08357bb
Zach Eriksen <zmeriksen@icloud.com>
2022-03-05 13:24:16 -0700

    Add File implementation

--- Sources/ScreenDataNavigation/ScreenDataNavigation.swift
+++ Sources/ScreenDataNavigation/ScreenDataNavigation.swift
@@ -106,6 +106,53 @@ public struct UserDefaultScreenProvider: ScreenProviding {
     }
 }
 
+@available(iOS 13.0, OSX 10.15, *)
+public struct FileScreenProvider: ScreenProviding {
+    public enum FileScreenProviderError: Error {
+        case noData
+    }
+    
+    public var baseKey: String
+    
+    public init(baseKey: String) {
+        self.baseKey = baseKey
+    }
+    
+    public func screen(forID id: String) -> AnyPublisher<SomeScreen, Error> {
+        Future { promise in
+            let path = FileManager.default.urls(
+                for: .documentDirectory,
+                   in: .userDomainMask
+            )[0].appendingPathComponent(key(forID: id))
+            
+            guard let data = try? Data(contentsOf: path) else {
+                promise(.failure(FileScreenProviderError.noData))
+                return
+            }
+            
+            do {
+                promise(.success(try JSONDecoder().decode(SomeScreen.self, from: data)))
+            } catch {
+                promise(.failure(error))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    public func key(forID id: String) -> String {
+        "\(baseKey)-\(id)"
+    }
+    
+    public func hasScreen(forId id: String) -> Bool {
+        let path = FileManager.default.urls(
+            for: .documentDirectory,
+               in: .userDomainMask
+        )[0].appendingPathComponent(key(forID: id))
+        
+        return (try? path.checkResourceIsReachable()) ?? false
+    }
+}
+
 // MARK: ScreenStoring
 @available(iOS 13.0, OSX 10.15, *)
 public protocol ScreenStoring {
@@ -129,6 +176,41 @@ public struct UserDefaultScreenStorer: ScreenStoring {
                     let key = self.key(forID: screen.id ?? "")
                     UserDefaults.standard.set(data,
                                               forKey: key)
+                }
+                promise(.success(()))
+            } catch {
+                promise(.failure(error))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    public func key(forID id: String) -> String {
+        "\(baseKey)-\(id)"
+    }
+}
+
+// MARK: ScreenStoring File Implementation
+@available(iOS 13.0, OSX 10.15, *)
+public struct FileScreenStorer: ScreenStoring {
+    public var baseKey: String
+    
+    public init(baseKey: String) {
+        self.baseKey = baseKey
+    }
+    
+    public func store(screens: [SomeScreen]) -> AnyPublisher<Void, Error> {
+        Future { promise in
+            do {
+                try screens.forEach { screen in
+                    let data = try JSONEncoder().encode(screen)
+                    let key = self.key(forID: screen.id ?? "")
+                    let path = FileManager.default.urls(
+                        for: .documentDirectory,
+                           in: .userDomainMask
+                    )[0].appendingPathComponent(key)
+                    
+                    try data.write(to: path)
                 }
                 promise(.success(()))
             } catch {
